93b94239e6f58fcc3757ecda90bdf395b20370f3
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-11-29 04:04:09 +0100

    Impl increment and get with redis

--- httprateredis.go
+++ httprateredis.go
@@ -1,6 +1,8 @@
 package httprateredis
 
 import (
+	"context"
+	"strconv"
 	"sync"
 	"time"
 
@@ -8,40 +10,61 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-type RedisRateLimiter struct {
-	inner     *redis.Client
+type redisRateLimiter struct {
+	inner        *redis.Client
 	mu           sync.Mutex
+	windowLength time.Duration
 }
 
-func NewRedisRateLimiter(c *redis.Client) *RedisRateLimiter {
-	return newRedisRateLimiter(c)
+func NewRedisRateLimiter(c *redis.Client, windowLength time.Duration) *redisRateLimiter {
+	return newRedisRateLimiter(c, windowLength)
 }
 
-func newRedisRateLimiter(c *redis.Client) *RedisRateLimiter {
-	return &RedisRateLimiter{
-		inner: c,
+func newRedisRateLimiter(c *redis.Client, windowLength time.Duration) *redisRateLimiter {
+	return &redisRateLimiter{
+		inner:        c,
+		windowLength: windowLength,
 	}
 }
 
-func (c *RedisRateLimiter) Increment(key string, currentWindow time.Time) error {
-	c.evict()
+func (c *redisRateLimiter) Increment(key string, currentWindow time.Time) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
+	hkey := string(httprate.LimitCounterKey(key, currentWindow))
+
+	c.inner.Incr(context.TODO(), hkey)
+	c.inner.Expire(context.TODO(), hkey, c.windowLength)
 	return nil
 }
 
-func (c *RedisRateLimiter) Get(key string, currentWindow, previousWindow time.Time) (int, int, error) {
+func (c *redisRateLimiter) Get(key string, currentWindow, previousWindow time.Time) (int, int, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	return 0,0, nil
+	curr, err := c.inner.Get(context.TODO(), string(httprate.LimitCounterKey(key, currentWindow))).Result()
+
+	if err != nil {
+		return 0, 0, err
+	}
+
+	prev, err := c.inner.Get(context.TODO(), string(httprate.LimitCounterKey(key, previousWindow))).Result()
+
+	if err != nil {
+		return 0, 0, err
+	}
+
+	currInt, err := strconv.Atoi(curr)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	prevInt, err := strconv.Atoi(prev)
+	if err != nil {
+		return currInt, 0, err
+	}
+
+	return currInt, prevInt, nil
 }
 
-func (c *RedisRateLimiter) evict() {
-	c.mu.Lock()
-
-	defer c.mu.Unlock()
-}
-
-
-var _ httprate.LimitCounter = &RedisRateLimiter{}
+var _ httprate.LimitCounter = &redisRateLimiter{}
